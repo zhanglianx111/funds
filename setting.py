a3870ca8d33f9a0e533765bb76941be8667f6945
zhanglianx111 <zhanglianx111@aliyun.com>
2015-02-17 09:55:19 +0800

    change G_NUMBER

--- setting.py
+++ setting.py
@@ -3,7 +3,7 @@
 
 G_URL = 'http://fund.eastmoney.com/Data/Fund_JJJZ_Data.aspx?t=1&lx=1&letter=&gsid=&text=&sort=zdf,desc&page=1,'
 G_OTHERS = '&dt=1424049754634&atfc=%onlySale=0'
-G_NUMBER = '1000'
+G_NUMBER = '2500'
 
 G_JJDM = 0
 G_JJMZ = 1
