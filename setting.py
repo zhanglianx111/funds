32a3381e7ea2d20c86098082a42dae922c31b895
zhanglianx111 <zhanglianx111@aliyun.com>
2015-02-17 11:52:47 +0800

    add index,daily,buy table and row name

--- setting.py
+++ setting.py
@@ -9,3 +9,25 @@ G_JJDM = 0
 G_JJMZ = 1
 G_DWJZ = 3
 G_SGZT = 9
+
+G_DB_FUND = 'fund'
+
+G_TABLE_FUND_INDEX = 'index'
+G_TABLE_RECORD_DAILY = 'daily'
+G_TABLE_FUNDS_BOUGHT = 'buy'
+
+# 下列字段用于buy表 
+# 买入基金代码
+G_FUND_INDEX = 'fund_index'
+
+# 买入金额
+G_MONEY = 'money'
+
+# 购买日期
+G_DATE_BUY = 'buy'
+
+# 卖出日期
+G_DATE_SELL = 'sell'
+
+# 购入份额 
+G_FUND_UNIT = 'unit'
