832a0cc614b876aeb1222efebecc18e2cd857fb0
zhanglianx111 <zhanglianx111@aliyun.com>
2015-02-25 10:28:39 +0800

    add log

---
+++ log/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+import logging
+from setting import  G_LOG_NAME, G_LOG_DIR 
+
+logging.basicConfig(filename = G_LOG_DIR + G_LOG_NAME, level = logging.INFO, filemode = 'a', format = '[%(levelname)s-%(module)s(%(lineno)d)] %(message)s')
+
+def getMyLogger(name):
+    logger = logging.getLogger(name)
+    return logger
+
