58a8781d68f8c0f2d906dee9c8db585e76397745
zhanglianx111 <zhanglianx111@aliyun.com>
2015-02-25 14:44:03 +0800

    change fund setting

--- setting.py
+++ setting.py
@@ -12,15 +12,15 @@ G_JJMZ = 1
 G_DWJZ = 3
 G_SGZT = 9
 
-G_DB_FUND = 'fund'
+G_DB_FUNDS = 'funds'
 
-G_TABLE_FUND_INDEX = 'index'
+G_TABLE_FUNDS_INDEX = 'index'
 G_TABLE_RECORD_DAILY = 'daily'
 G_TABLE_FUNDS_BOUGHT = 'buy'
 
 # 下列字段用于buy表 
 # 买入基金代码
-G_FUND_INDEX = 'fund_index'
+G_FUNDS_INDEX = 'funds_index'
 
 # 买入金额
 G_MONEY = 'money'
@@ -39,3 +39,7 @@ G_FUND_UNIT = 'unit'
 G_LOG_NAME = 'funds.log' 
 G_LOG_DIR = '/var/log/'
 #G_LOG_LEVEL = logging.INFO
+
+# mongodb
+G_HOST = '127.0.0.1'
+G_PORT = 27017
