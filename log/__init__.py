79752851d6f0b80994fe7e4111185f1b6c8371cf
zhanglianx111 <zhanglianx111@aliyun.com>
2015-02-28 15:08:52 +0800

    add time in log

--- log/__init__.py
+++ log/__init__.py
@@ -3,7 +3,7 @@
 import logging
 from setting import  G_LOG_NAME, G_LOG_DIR 
 
-logging.basicConfig(filename = G_LOG_DIR + G_LOG_NAME, level = logging.INFO, filemode = 'a', format = '[%(levelname)s-%(module)s(%(lineno)d)] %(message)s')
+logging.basicConfig(filename = G_LOG_DIR + G_LOG_NAME, level = logging.INFO, filemode = 'a', format = '[%(asctime)s-%(levelname)s-%(module)s(%(lineno)d)] %(message)s')
 
 def getMyLogger(name):
     logger = logging.getLogger(name)
