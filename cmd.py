0992d9888ef4ef6b1256a3187bf0c28300ab2100
zhanglianx111 <zhanglianx111@aliyun.com>
2015-03-12 17:35:38 +0800

    增加命令行文件夹

--- cmd.py
+++ cmd.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import sys
+from optparse import OptionParser
+
+if __name__ == '__main__':
+    print sys.argv
+    command = sys.argv[1]
+    if command == "delete":
+        print command
+        args = sys.argv[2:]
+        print args
+    elif command == "clear":
+        print command
+    elif command == "calculate":
+        print command
+    else:
+        print "not command %s" % (command)
+    sys.exit(0)
+    op = OptionParser()
+    print op
+    op.add_option(
+        "--db",
+        "--database",
+        action="store",
+        type="string",
+        help="delete database",
+        dest="db_name")
+    options, args = op.parse_args()
+    print options.db_name
+    print args
+
